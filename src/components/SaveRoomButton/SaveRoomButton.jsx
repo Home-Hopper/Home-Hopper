1a8fc93c33fdc9c272e06ca346101cb8548cd43f
nerea pardo <nerea.pardo.cundins@gmail.com>
2022-01-26 14:38:01 +0000

    change save room button

--- src/components/SaveRoomButton/SaveRoomButton.jsx
+++ src/components/SaveRoomButton/SaveRoomButton.jsx
@@ -9,24 +9,28 @@ import * as PATHS from '../../utils/paths'
 import { Link } from 'react-router-dom'
 
 const SaveRoomButton = ({ roomId }) => {
-  const [icon, setIcon] = useState(faHeart)
+  const [icon, setIcon] = useState('emptyHeart')
   const { user, setUser } = useContext(AuthContext)
   const iconStyle = {
     ...(icon === faHeart && { color: 'tomato' }),
   }
+  const heartIcon = {
+    emptyHeart: faEmptyHeart,
+    heart: faHeart,
+  }
 
   useEffect(() => {
     if (user?.savedRooms.includes(roomId)) {
-      setIcon(faHeart)
+      setIcon('heart')
     } else {
-      setIcon(faEmptyHeart)
+      setIcon('emptyHeart')
     }
   }, [roomId, user?.savedRooms])
 
   const updateSaved = async () => {
-    if (icon === faHeart) setIcon(faEmptyHeart)
+    if (icon === 'heart') setIcon('emptyHeart')
     else {
-      setIcon(faHeart)
+      setIcon('heart')
     }
     const res = await updateSavedRooms(user._id, roomId)
     setUser({ ...res.data.user })
@@ -37,7 +41,7 @@ const SaveRoomButton = ({ roomId }) => {
       {user && (
         <button onClick={updateSaved}>
           <FontAwesomeIcon
-            icon={icon}
+            icon={heartIcon[icon]}
             className="text-2xl text-gray-700"
             style={iconStyle}
           />
@@ -46,7 +50,7 @@ const SaveRoomButton = ({ roomId }) => {
       {!user && (
         <Link to={PATHS.LOGINPAGE}>
           <FontAwesomeIcon
-            icon={faEmptyHeart}
+            icon={heartIcon.emptyHeart}
             className="text-2xl text-gray-700"
           />
         </Link>
