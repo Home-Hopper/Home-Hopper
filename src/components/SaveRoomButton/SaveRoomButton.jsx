6a3bbe4399840d3b24cc50ea07fe1f70e7e945fd
nerea pardo <nerea.pardo.cundins@gmail.com>
2022-01-26 18:50:26 +0000

    deploy

--- src/components/SaveRoomButton/SaveRoomButton.jsx
+++ src/components/SaveRoomButton/SaveRoomButton.jsx
@@ -1,8 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart } from '@fortawesome/free-solid-svg-icons'
 import { faHeart as faEmptyHeart } from '@fortawesome/free-regular-svg-icons'
-import { useEffect, useState } from 'react/cjs/react.development'
-import { useContext } from 'react'
+import { useEffect, useState, useContext } from 'react'
 import { AuthContext } from '../../context/auth.context'
 import { updateSavedRooms } from '../../services/rooms.services'
 import * as PATHS from '../../utils/paths'
@@ -11,41 +10,39 @@ import { Link } from 'react-router-dom'
 const SaveRoomButton = ({ roomId }) => {
   const [icon, setIcon] = useState('')
   const { user, setUser } = useContext(AuthContext)
-  // const iconStyle = {
-  //   ...(icon === 'heart' && { color: 'tomato' }),
-  // }
+  const iconStyle = {
+    ...(icon === 'heart' && { color: 'tomato' }),
+  }
   const heartIcon = {
     emptyHeart: faEmptyHeart,
     heart: faHeart,
   }
 
-  // useEffect(() => {
-  //   if (user?.savedRooms.includes(roomId)) {
-  //     setIcon('heart')
-  //   } else {
-  //     setIcon('emptyHeart')
-  //   }
-  // }, [roomId, user?.savedRooms])
+  useEffect(() => {
+    if (user?.savedRooms.includes(roomId)) {
+      setIcon('heart')
+    } else {
+      setIcon('emptyHeart')
+    }
+  }, [roomId, user?.savedRooms])
 
-  // const updateSaved = async () => {
-  //   if (icon === 'heart') setIcon('emptyHeart')
-  //   else {
-  //     setIcon('heart')
-  //   }
-  //   const res = await updateSavedRooms(user._id, roomId)
-  //   setUser({ ...res.data.user })
-  // }
+  const updateSaved = async () => {
+    if (icon === 'heart') setIcon('emptyHeart')
+    else {
+      setIcon('heart')
+    }
+    const res = await updateSavedRooms(user._id, roomId)
+    setUser({ ...res.data.user })
+  }
 
   return (
     <>
       {user && (
-        <button
-        //  onClick={updateSaved}
-        >
+        <button onClick={updateSaved}>
           <FontAwesomeIcon
-            icon={heartIcon.heart}
+            icon={heartIcon[icon]}
             className="text-2xl text-gray-700"
-            // style={iconStyle}
+            style={iconStyle}
           />
         </button>
       )}
