b5045babbd5243dedd6523d2baa12b087ef21780
nerea pardo <nerea.pardo.cundins@gmail.com>
2022-01-26 17:42:56 +0000

    test

--- src/components/SaveRoomButton/SaveRoomButton.jsx
+++ src/components/SaveRoomButton/SaveRoomButton.jsx
@@ -9,54 +9,52 @@ import * as PATHS from '../../utils/paths'
 import { Link } from 'react-router-dom'
 
 const SaveRoomButton = ({ roomId }) => {
-  // const [icon, setIcon] = useState('emptyHeart')
+  const [icon, setIcon] = useState('emptyHeart')
   const { user, setUser } = useContext(AuthContext)
-  // const iconStyle = {
-  //   ...(icon === 'heart' && { color: 'tomato' }),
-  // }
-  // const heartIcon = {
-  //   emptyHeart: faEmptyHeart,
-  //   heart: faHeart,
-  // }
+  const iconStyle = {
+    ...(icon === 'heart' && { color: 'tomato' }),
+  }
+  const heartIcon = {
+    emptyHeart: faEmptyHeart,
+    heart: faHeart,
+  }
 
-  // useEffect(() => {
-  //   if (user?.savedRooms.includes(roomId)) {
-  //     setIcon('heart')
-  //   } else {
-  //     setIcon('emptyHeart')
-  //   }
-  // }, [roomId, user?.savedRooms])
+  useEffect(() => {
+    if (user?.savedRooms.includes(roomId)) {
+      setIcon('heart')
+    } else {
+      setIcon('emptyHeart')
+    }
+  }, [roomId, user?.savedRooms])
 
-  // const updateSaved = async () => {
-  //   if (icon === 'heart') setIcon('emptyHeart')
-  //   else {
-  //     setIcon('heart')
-  //   }
-  //   const res = await updateSavedRooms(user._id, roomId)
-  //   setUser({ ...res.data.user })
-  // }
+  const updateSaved = async () => {
+    if (icon === 'heart') setIcon('emptyHeart')
+    else {
+      setIcon('heart')
+    }
+    const res = await updateSavedRooms(user._id, roomId)
+    setUser({ ...res.data.user })
+  }
 
   return (
     <>
-      {user &&
-        {
-          /* <button onClick={updateSaved}>
+      {user && (
+        <button onClick={updateSaved}>
           <FontAwesomeIcon
             icon={heartIcon[icon]}
             className="text-2xl text-gray-700"
             style={iconStyle}
           />
-        </button> */
-        }}
-      {!user &&
-        {
-          /* <Link to={PATHS.LOGINPAGE}>
+        </button>
+      )}
+      {!user && (
+        <Link to={PATHS.LOGINPAGE}>
           <FontAwesomeIcon
             icon={heartIcon.emptyHeart}
             className="text-2xl text-gray-700"
           />
-        </Link> */
-        }}
+        </Link>
+      )}
     </>
   )
 }
