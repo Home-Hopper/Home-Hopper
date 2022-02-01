5f0008bc4f1eb8f3950333ff758491a37e62b1af
nerea pardo <nerea.pardo.cundins@gmail.com>
2022-02-01 12:07:16 +0000

    fix demo user info toast

--- src/components/DemoToast/DemoToast.js
+++ src/components/DemoToast/DemoToast.js
@@ -5,28 +5,29 @@ const DemoToast = () => {
   let [isOpen, setIsOpen] = useState(false)
   const { user } = useContext(AuthContext)
   useEffect(() => {
-    if (!user && localStorage.getItem('toast') !== 'closed') setIsOpen(true)
+    if (!user && localStorage.getItem('demoUserLogin') !== 'closed')
+      setIsOpen(true)
   }, [user])
 
   const manageToast = () => {
     setIsOpen(false)
-    localStorage.setItem('toast', 'closed')
+    localStorage.setItem('demoUserLogin', 'closed')
   }
   return (
     <div
-      className={`h-screen w-screen top-0 fixed z-50 ${isOpen ? '' : 'hidden'}`}
+      className={`fixed  w-full sm:w-96 h-24 bg-white shadow-2xl sm:rounded-lg border border-solid border-gray-500  bottom-0 md:bottom-4 right-0 md:right-7   py-4 px-2 z-50 ${
+        isOpen ? '' : 'hidden'
+      } `}
     >
-      <div className="bg-white shadow-2xl md:rounded-lg border border-solid border-gray-500 w-full md:w-96 h-24 absolute  bottom-0 md:bottom-4 right-0 md:right-7 py-4 px-2">
-        <button
-          className="absolute top-0 right-4 text-purple-dark font-bold text-xl"
-          onClick={manageToast}
-        >
-          x
-        </button>
-        <p>Use this user for demo purposes:</p>
-        <p className="ml-4">Email: demo@user.com</p>
-        <p className="ml-4">Password: Demopassword123</p>
-      </div>
+      <button
+        className="absolute top-0 right-4 text-purple-dark font-bold text-xl"
+        onClick={manageToast}
+      >
+        x
+      </button>
+      <p>Use this user for demo purposes:</p>
+      <p className="ml-4">Email: demo@user.com</p>
+      <p className="ml-4">Password: Demopassword123</p>
     </div>
   )
 }
